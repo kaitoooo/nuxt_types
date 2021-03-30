e78b46ca0ad1e1e6d4ed80344c10bedebbf6f78a
KaitoTakase <k-takase@vectorinc.co.jp>
2021-03-30 11:27:37 +0900

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="container">
+        <div>
+            <Logo />
+            <h1 class="title">
+                template-nuxt-types
+            </h1>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({});
+</script>
